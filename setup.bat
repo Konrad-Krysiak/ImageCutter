66957b142f3072a6cecbd98392e3edb98c4582e8
Konrad-Krysiak <konradkrysiak17@gmail.com>
2016-05-30 23:06:12 +0200

    fix

--- setup.bat
+++ setup.bat
@@ -3,14 +3,14 @@ cd /d %~dp0\resources
 .\python-2.7.11.msi
 echo -installing python
 
-set PYTHONPATH=%PYTHONPATH%;C:\My_python_lib
+set PYTHONPATH=%PYTHONPATH%;C:\Python27
 echo -adding PYTHON environment variable to PYTHONPATH
 setx path "%path%;C:\Python27;"
 echo -adding PYTHON environment variable to PATH
 
 python get-pip.py 
 echo -installing pip
-setx path "%path%;C:\Python\Scripts;"
+setx path "%path%;C:\Python27\Scripts;"
 echo -adding PIP environment variable to PATH
 
 pip install Pillow
